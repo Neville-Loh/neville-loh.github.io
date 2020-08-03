bb5e556f48c5d562bbf3bde9b3ed7a5d3ffaab6d
Neville <nevilleloh0115@gmail.com>
2020-08-03 17:29:22 +1200

    test2.html upadte 1

--- test2.js
+++ test2.js
@@ -14,6 +14,7 @@ function animatedForm(){
             // check for validation
                 if(input.type === "text" && validateUser(input)){
                     console.log(input.value);
+                    test(input.value);
                     console.log("everything is okay!");
                 }
         });
@@ -34,4 +35,39 @@ function validateUser(user){
 function error(color){
     document.body.style.backgroundColor = color;
 }
+
+function review_encode(text) {
+    var arr = string_to_arr(text);
+    var encoded = [1];
+    var count = 0;
+    for (word of arr) {
+        encoded.push(word_index[word]);
+        if (count >= 250) {
+            break;
+        }
+        count++;
+    }
+    if (encoded.length < 250) {
+        for (var i = 0; i < (249 - count); i++) {
+            encoded.push(word_index["<PAD>"]);
+        }
+    }
+    return encoded;
+
+}
+function string_to_arr(s) {
+    return s.replace(/[\W_]+/g, " ").split(" ");
+}
+
+async function test(my_input){
+    var encoded = review_encode(my_input);
+    const model = await tf.loadLayersModel('./projects/tf_start/jsModel/model.json');
+    console.log(encoded);
+    var result = model.predict(tf.tensor([encoded])).array();
+    alert(result);
+    result.then(value => console.log(value));
+}
+
+
+console.log("update1");
 animatedForm();
